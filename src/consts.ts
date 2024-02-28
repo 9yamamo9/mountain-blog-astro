be5e91f43e05b588805f41632caf922302364201
yutaYAMAMORI <yamamori929yuta@gmail.com>
2024-02-28 15:13:01 +0900

    mod: change the blog title.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mountain Forest';
+export const SITE_DESCRIPTION = 'This site is the blog about technologies and life';
