ae5839f87b922cef46e0ce7ab391828c0c63947e
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-05 11:36:13 +0900

    add: astro-embed for Youtube and X.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import embeds from 'astro-embed/integration'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+	site: 'https://example.com',
+	integrations: [embeds(), mdx(), sitemap(), tailwind()]
+})
