5a2690256b2050a2e58428c0f363cd36018d429f
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-19 15:34:14 +0900

    add: integrate React.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import embeds from 'astro-embed/integration'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import embeds from 'astro-embed/integration';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mountain-forest-blog.pages.dev',
-	integrations: [embeds(), mdx(), sitemap(), tailwind()]
-})
+  site: 'https://mountain-forest-blog.pages.dev',
+  integrations: [embeds(), mdx(), sitemap(), tailwind(), react()]
+});
